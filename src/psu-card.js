8f583c7f29c98d57923f73ed2197c84e72bf1237
btopro <btopro@outlook.com>
2023-09-28 11:47:29 -0400

    recording righ tbeforee class

--- src/psu-card.js
+++ src/psu-card.js
@@ -98,6 +98,27 @@ class PsuCard extends LitElement {
     display: none;
       }
     }
+
+    .hidden {
+      display: none;
+  }
+  .cards {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+  }
+  .card-container {
+      background-color: #001e44;
+      width: 20%;
+      margin: 8px;
+      padding: 10px;
+      border-radius: 12px;
+  }
+  .card-content img {
+      max-width: 100%;
+      border-radius: 10px;
+  }
+
   `;
 
   constructor() {
@@ -105,57 +126,85 @@ class PsuCard extends LitElement {
     this.header = 'My app';
   }
 
+  firstUpdated() {
+    const titleBtn = this.shadowRoot.querySelector('#titleBtn');
+    const deleteBtn = this.shadowRoot.querySelector('#deleteBtn');
+    const cardTitle = this.shadowRoot.querySelector('#cardTitle');
+    const cardDescription = this.shadowRoot.querySelector('#cDescription');
+    const toggleDescription = this.shadowRoot.querySelector('#toggleDescription');
+    const btn = this.shadowRoot.querySelector('#btn');
+    const card = this.shadowRoot.querySelector('.card-container');
+    const clone = card.cloneNode(true);
+    const cards = this.shadowRoot.querySelector('.cards');
+    const details = this.shadowRoot.querySelector('details');
+    const summary = this.shadowRoot.querySelector('summary');
+    const cardContent = this.shadowRoot.querySelector('.card-content');
+    const cardImage = this.shadowRoot.querySelector('img');
+    const cardContainer = this.shadowRoot.querySelector('.card-container');
+    const cardClone = this.shadowRoot.querySelector('.card-container');
+    const cardClone2 = this.shadowRoot.querySelector('.card-container');
+
+    titleBtn.addEventListener('click', () => {
+      cardTitle.innerHTML = 'Penn State Football';
+    });
+
+    deleteBtn.addEventListener('click', () => {
+      cards.removeChild(cardClone2);
+    });
+
+    toggleDescription.addEventListener('click', () => {
+      cardDescription.classList.toggle('hidden');
+    });
+
+    btn.addEventListener('click', () => {
+      cards.appendChild(clone);
+    });
+
+    cardContent.addEventListener('click', () => {
+      cardImage.classList.toggle('hidden');
+    });
+
+    summary.addEventListener('click', () => {
+      cardDescription.classList.toggle('hidden');
+    });
+  }
+
+  randomColorGenerator(e) {
+    console.warn(e);
+    const cardContainer = this.shadowRoot.querySelector('.card-container');
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    cardContainer.style.backgroundColor = '#' + randomColor;
+    return randomColor;
+  }
+
+  cloneCard(e) {
+    const card = this.shadowRoot.querySelector('.card-container');
+    const clone = card.cloneNode(true);
+    this.shadowRoot.querySelector('.cards').appendChild(clone);
+  }
+
   render() {
     return html`
-  <button id="btn">Clone Card</button>
-  <button id="colorBtn">Change Card Color</button>
+  <button id="btn" @click="${this.cloneCard}">Clone Card</button>
+  <button @click="${this.randomColorGenerator}">Change Card Color</button>
   <button id="titleBtn">Change Title</button>
   <button id="deleteBtn">Delete Last Card</button>
-    <div class="cards">
-    <div class="card-container">
-      <div class="card-content">
-        <h2 id="cardTitle">Penn State Blue and White Game</h2>
-        <p>Blue and White game day</p>
-        <img src="https://www.pennlive.com/resizer/nmIUnuL_9rmAqjXOlD5yFnxF29I=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/BHM62A6AUJFHVJIZHI6BDPJHWY.jpg" alt="Card Image">
-        <div>
-
-            <style>
-        .hidden {
-            display: none;
-        }
-        .cards {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-        }
-        .card-container {
-            background-color: #001e44;
-            width: 20%;
-            margin: 8px;
-            padding: 10px;
-            border-radius: 12px;
-        }
-        .card-content img {
-            max-width: 100%;
-            border-radius: 10px;
-        }
-
-        
-    </style>
- </head>
-            
-      
-        </div>
-      </div>
-      <details>
-        
-      <summary id = "toggleDescription"  > Toggle Description </summary>
-      <p id = "cDescription" >110 thousand fans come to watch penn state football to create the greatest show in college football.</p>
-
-      </details>
-     
+  <div class="cards">
+  <div class="card-container">
+    <div class="card-content">
+      <h2 id="cardTitle">Penn State Blue and White Game</h2>
+      <p>Blue and White game day</p>
+      <img src="https://www.pennlive.com/resizer/nmIUnuL_9rmAqjXOlD5yFnxF29I=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/BHM62A6AUJFHVJIZHI6BDPJHWY.jpg" alt="Card Image" />
     </div>
+    <details>
+      
+    <summary id="toggleDescription"> Toggle Description </summary>
+    <p id="cDescription" >110 thousand fans come to watch penn state football to create the greatest show in college football.</p>
+
+    </details>
+  
   </div>
+</div>
     `;
   }
 }
